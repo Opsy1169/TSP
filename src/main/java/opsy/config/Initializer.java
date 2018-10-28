6c8f540888132c63703ac9ff54a85c6369fc0707
Opsy1169 <mynewsuper@gmail.com>
2018-10-28 17:30:35 +0400

    ХМЛ конфиг для видимости и нормальной работы с переменными на жсп

--- src/main/java/opsy/config/Initializer.java
+++ src/main/java/opsy/config/Initializer.java
@@ -1,34 +1,34 @@
-package opsy.config;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.request.RequestContextListener;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
-public class Initializer implements WebApplicationInitializer {
-    /**
-     * После добавления второго файла конфигурации пришлось завести такой класс
-     * Он является аналогом веб.хмл и обшего конфигурационного файла, в общем тут вообще все, кроме фильтров
-     * Фильтры, в том числе необходмый для безопасности, лежат в веб.хмл
-     * @param servletContext
-     * @throws ServletException
-     */
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-        ctx.register(MvcConfiguration.class);
-        ctx.register(SecurityConfiguration.class);
-        servletContext.addListener(new ContextLoaderListener(ctx));
-        ctx.setServletContext(servletContext);
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispacther", new DispatcherServlet(ctx));
-        servlet.addMapping("/");
-        servlet.setLoadOnStartup(1);
-    }
-}
+//package opsy.config;
+//
+//import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.web.WebApplicationInitializer;
+//import org.springframework.web.context.ContextLoaderListener;
+//import org.springframework.web.context.WebApplicationContext;
+//import org.springframework.web.context.request.RequestContextListener;
+//import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+//import org.springframework.web.servlet.DispatcherServlet;
+//
+//import javax.servlet.ServletContext;
+//import javax.servlet.ServletException;
+//import javax.servlet.ServletRegistration;
+//
+//public class Initializer implements WebApplicationInitializer {
+//    /**
+//     * После добавления второго файла конфигурации пришлось завести такой класс
+//     * Он является аналогом веб.хмл и обшего конфигурационного файла, в общем тут вообще все, кроме фильтров
+//     * Фильтры, в том числе необходмый для безопасности, лежат в веб.хмл
+//     * @param servletContext
+//     * @throws ServletException
+//     */
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+//        ctx.register(MvcConfiguration.class);
+//        ctx.register(SecurityConfiguration.class);
+//        servletContext.addListener(new ContextLoaderListener(ctx));
+//        ctx.setServletContext(servletContext);
+//        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispacther", new DispatcherServlet(ctx));
+//        servlet.addMapping("/");
+//        servlet.setLoadOnStartup(1);
+//    }
+//}
